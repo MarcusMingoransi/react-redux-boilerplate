0565c5601193ed5134291dcebc38488ad8a0cce9
Marcus Vinicius Mingoransi <marcus.mingoransi@outlook.com>
2021-03-10 18:03:20 -0300

    feat: Add eslint and prettier

---
+++ app/.eslintrc.js
@@ -0,0 +1,82 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier',
+    'prettier/react',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+
+    'import/prefer-default-export': 'off',
+    'import/order': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    'class-methods-use-this': 'off',
+    'no-plusplus': 'off',
+    'no-continue': 'off',
+    'no-param-reassign': 'off',
+    'no-use-before-define': 'off',
+    redix: 'off',
+    'no-bitwise': 'off',
+    'no-unused-expressions': 'off',
+    'no-restricted-globals': 'off',
+    'no-muilti-assign': 'off',
+    camelcase: 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
