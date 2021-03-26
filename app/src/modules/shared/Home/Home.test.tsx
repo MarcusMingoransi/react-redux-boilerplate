ca5d4f3f2d1fe3f63a938e26b9e12c84b2db55ab
Marcus Vinicius Mingoransi <marcus.mingoransi@outlook.com>
2021-03-25 21:48:14 -0300

    feat: Add Jest

--- app/src/modules/shared/Home/Home.test.tsx
+++ app/src/modules/shared/Home/Home.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import App from './Home'
+import Home from './Home'
+import 'jest'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, configure } from 'enzyme'
+
+configure({ adapter: new Adapter() })
+
+const wrapper = shallow(<Home />)
+
+describe('HomeComponent - No props', () => {
+  it('should have state set properly', () => {
+    expect(wrapper.find('button').text).toEqual('Click to Subtract')
+  })
+})
+
+const sum = (a: number, b: number) => a + b
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3)
 })
--- app/src/modules/shared/Home/index.ts
+++ app/src/modules/shared/Home/index.ts
@@ -1,2 +1,3 @@
+import './Home.css'
 import Home from './Home'
 export default Home
